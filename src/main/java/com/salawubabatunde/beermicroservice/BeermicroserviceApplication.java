9257c15c861b765b20921c83296757862998f9bd
Salawu Babatunde Afees <fisco57@yahoo.com>
2023-10-26 10:15:27 +0100

    first commit

---
+++ src/main/java/com/salawubabatunde/beermicroservice/BeermicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.salawubabatunde.beermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeermicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeermicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salawubabatunde/beermicroservice/BeermicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salawubabatunde.beermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeermicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
