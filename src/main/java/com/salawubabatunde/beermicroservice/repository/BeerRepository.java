99a378f7b508d26691af770443aaf8cb6e8c1e69
Salawu Babatunde Afees <fisco57@yahoo.com>
2023-10-28 00:25:53 +0100

    added bootstrap data - closes #7

--- src/main/java/com/salawubabatunde/beermicroservice/repository/BeerRepository.java
+++ src/main/java/com/salawubabatunde/beermicroservice/repository/BeerRepository.java
@@ -1,9 +1,9 @@
 package com.salawubabatunde.beermicroservice.repository;
 
 import com.salawubabatunde.beermicroservice.domain.Beer;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.UUID;
 
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+public interface BeerRepository extends CrudRepository<Beer, UUID> {
 }
