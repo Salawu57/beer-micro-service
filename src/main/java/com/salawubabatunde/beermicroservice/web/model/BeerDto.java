6de3fcb807092b13442b6f305003043735974525
Salawu Babatunde Afees <fisco57@yahoo.com>
2023-11-07 15:06:52 +0100

    adding json creator - closes #12, closes #13

--- src/main/java/com/salawubabatunde/beermicroservice/web/model/BeerDto.java
+++ src/main/java/com/salawubabatunde/beermicroservice/web/model/BeerDto.java
@@ -1,9 +1,7 @@
 package com.salawubabatunde.beermicroservice.web.model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Null;
-import jakarta.validation.constraints.Positive;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -25,13 +23,16 @@ public class BeerDto {
     @Null
     private Integer version;
 
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     @Null
     private OffsetDateTime createdDate;
 
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     @Null
     private OffsetDateTime lastModifiedDate;
 
     @NotBlank
+    @Size(min=3, max = 100)
     private String beerName;
 
     @NotNull
@@ -41,10 +42,17 @@ public class BeerDto {
     @NotNull
     private Long upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
+
+    @Positive
     private Integer quantityOnHand;
+
+    @Positive
     private Integer minOnHand;
+
+    @Positive
     private Integer quantityToBrew;
 }
--- src/test/java/com/salawubabatunde/beermicroservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/salawubabatunde/beermicroservice/web/controller/BeerControllerTest.java
@@ -5,19 +5,31 @@ import com.salawubabatunde.beermicroservice.web.model.BeerDto;
 import com.salawubabatunde.beermicroservice.web.model.BeerStyleEnum;
 import lombok.val;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.constraints.ConstraintDescriptions;
+import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.StringUtils;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "dev.springframework.guru", uriPort = 8080)
 @WebMvcTest(BeerController.class)
 class BeerControllerTest {
     @Autowired
@@ -28,19 +40,58 @@ class BeerControllerTest {
 
     @Test
     void getBeerById() throws Exception {
-     mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString())
+     mockMvc.perform(get("/api/v1/beer/{beerId}", UUID.randomUUID().toString())
+              .param("iscold","yes")
              .accept(MediaType.APPLICATION_JSON))
-             .andExpect(status().isOk());
+             .andExpect(status().isOk())
+             .andDo(document("v1/beer-get",
+                     pathParameters(
+                         parameterWithName("beerId").description("UUID of desired beer to get.")
+             ),
+                     queryParameters(
+                             parameterWithName("iscold").description("is beer cold query param")
+              ),
+                     responseFields(
+                             fieldWithPath("id").description("id of Beer"),
+                             fieldWithPath("version").description("version number"),
+                             fieldWithPath("createdDate").description("Date Created"),
+                             fieldWithPath("lastModifiedDate").description("Date Updated"),
+                             fieldWithPath("beerName").description("Beer Name"),
+                             fieldWithPath("beerStyle").description("Beer Style"),
+                             fieldWithPath("upc").description("Upc of Beer"),
+                             fieldWithPath("price").description("Price"),
+                             fieldWithPath("quantityOnHand").description("Quantity on hand"),
+                             fieldWithPath("minOnHand").description("Minimum on hand"),
+                             fieldWithPath("quantityToBrew").description("Quantity to Brew")
+                     )
+             ));
     }
 
     @Test
     void saveNewBeer() throws Exception{
         val beerDto = getValidBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+
+        ConstrainedFields fields = new ConstrainedFields(BeerDto.class);
+
         mockMvc.perform(post("/api/v1/beer")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(beerDtoJson))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andDo(document("v1/beer-new",
+                        requestFields(
+                                fields.withPath("id").ignored(),
+                                fields.withPath("version").ignored(),
+                                fields.withPath("createdDate").ignored(),
+                                fields.withPath("lastModifiedDate").ignored(),
+                                fields.withPath("beerName").description("Name of the beer"),
+                                fields.withPath("beerStyle").description("Style of Beer"),
+                                fields.withPath("upc").description("Beer UPC").attributes(),
+                                fields.withPath("price").description("Beer price"),
+                                fields.withPath("quantityOnHand").ignored(),
+                                fields.withPath("minOnHand").ignored(),
+                                fields.withPath("quantityToBrew").description("quantity to brew")
+                        )));
     }
 
     @Test
@@ -61,4 +112,22 @@ class BeerControllerTest {
                 .upc(121493485934L)
                 .build();
     }
+
+
+    private static class ConstrainedFields {
+
+        private final ConstraintDescriptions constraintDescriptions;
+
+        ConstrainedFields(Class<?> input) {
+            this.constraintDescriptions = new ConstraintDescriptions(input);
+        }
+
+        private FieldDescriptor withPath(String path) {
+            return fieldWithPath(path).attributes(key("constraints").value(StringUtils
+                    .collectionToDelimitedString(this.constraintDescriptions
+                            .descriptionsForProperty(path), ". ")));
+        }
+    }
+
+
 }
